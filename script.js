0845f80b80f1069da99665569498628e78f64a19
Alim <mardaliev712@gmail.com>
2023-02-21 23:20:59 +0400

    new files js

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+let numberofFilms;
+
+function start() {
+     numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+     while (numberofFilms == '' || numberofFilms == null || isNaN(numberofFilms)) {
+     numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
+     }
+}
+
+start();
+
+
 
 
 const personalMovieDB = {
@@ -9,12 +35,56 @@ const personalMovieDB = {
       privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10  && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+        } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+  } else {
+        alert('Нихуя не вышло, соре');
+  }
+}
+// detectPersonalLevel();
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+     personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');
+    }
+}
+writeYourGenres();
 
-console.log(personalMovieDB);
+function remberMyFilms() {
+    for(let i =0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+  
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+              personalMovieDB.movies[a] = b;
+              console.log('done');
+        } else {
+            console.log('error'); 
+            i--;
+        }
+  }
+}
+
+// remberMyFilms();
+
+// function showMyDB() {
+//     if (personalMovieDB.privat === false)  {
+//         console.log(personalMovieDB);
+//     }
+// }
+// showMyDB();
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB); 
+    }
+}
+ showMyDB(personalMovieDB.privat);
+
+
+// Код возьмите из предыдущего домашнего задания
